71b3f163ef4a21b0fb0fc7f10e7edd543d31e75a
Almas <almasnurlanov16@gmail.com>
2021-06-16 15:51:50 +0600

    Initial commit

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,9 @@
+import 'package:news_app_mvvm/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia{
+  final NumberTriviaRepository repository;
+
+  GetConcreteNumberTrivia(this.repository);
+
+
+}
